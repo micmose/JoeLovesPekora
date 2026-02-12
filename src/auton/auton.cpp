1a0bef21d362c92c75b2f62bab3ada1b68bfc66a
micmose <vujonathan2007>
2026-02-12 16:10:16 -0600

    More skills auto stuff

--- src/auton/auton.cpp
+++ src/auton/auton.cpp
@@ -5,93 +5,94 @@
 #include "pros/error.h"
 // #include <ios>
 void run_auto_skills() {
-   chassis.setPose(0, 0, 0);
-   wingerL.set_value(1);
-   wingerR.set_value(1);
-   /******************
-   CLEAR PARKING ZONE
-   *******************/
-   sucker.move(127);
-   chassis.moveToPoint(0, -5, 500, {.forwards = false, .minSpeed = 50});
-   chassis.moveToPoint(0, 16, 5000, {.minSpeed = 100});
+  chassis.setPose(0, 0, 0);
+  wingerL.set_value(1);
+  wingerR.set_value(1);
+  /******************
+  CLEAR PARKING ZONE
+  *******************/
+  sucker.move(127);
+  chassis.moveToPoint(0, -5, 500, {.forwards = false, .minSpeed = 50});
+  chassis.moveToPoint(0, 16, 5000, {.minSpeed = 100});
 
-   /******************
-   TRACK DISTANCE FROM WALL
-   *******************/
-   chassis.turnToHeading(90, 1000);
-   chassis.moveToPoint(12, chassis.getPose().y, 1000, {});
-   chassis.turnToHeading(180, 1000);
-   chassis.setBrakeMode(pros::E_MOTOR_BRAKE_BRAKE);
-   while (true) {
-     chassis.moveToPoint(chassis.getPose().x, chassis.getPose().y + 2, 500,
-                         {.forwards = false,
-                          .maxSpeed = 40,
-                          .minSpeed = 40,
-                          .earlyExitRange = 0.5},
-                         false);
-     if (lookout.get_distance() < 750) {
-       chassis.turnToHeading(270, 1000);
-       break;
-     }
-   }
+  /******************
+  TRACK DISTANCE FROM WALL
+  *******************/
+  chassis.turnToHeading(90, 1000);
+  chassis.moveToPoint(12, chassis.getPose().y, 1000, {});
+  chassis.turnToHeading(180, 1000);
+  chassis.setBrakeMode(pros::E_MOTOR_BRAKE_BRAKE);
+  while (true) {
+    chassis.moveToPoint(chassis.getPose().x, chassis.getPose().y + 2, 500,
+                        {.forwards = false,
+                         .maxSpeed = 40,
+                         .minSpeed = 40,
+                         .earlyExitRange = 0.5},
+                        false);
+    if (lookout.get_distance() < 750) {
+      chassis.turnToHeading(270, 1000);
+      break;
+    }
+  }
 
-   /******************
-   SCORE LONG GOAL
-   *******************/
-   chassis.moveToPoint(35, chassis.getPose().y + 8, 2500,
-                       {.forwards = false, .maxSpeed = 70, .minSpeed = 50});
-   pros::delay(1000);
-   hoodlum.move(127);
-   pros::delay(1000);
+  /******************
+  SCORE LONG GOAL
+  *******************/
+  chassis.moveToPoint(35, chassis.getPose().y + 8, 2500,
+                      {.forwards = false, .maxSpeed = 70, .minSpeed = 50});
+  pros::delay(1000);
+  hoodlum.move(127);
+  pros::delay(1000);
 
-   /******************
-   CLEAR RED LEFT MATCH LOADER
-   *******************/
-   chassis.setPose(0, 0, chassis.getPose().theta);
-   matcher.set_value(1);
-   chassis.moveToPose(-50, -3, 270, 2000, {.maxSpeed = 70, .minSpeed = 50});
-   hoodlum.move(0);
-   pros::delay(2500);
+  /******************
+  CLEAR RED LEFT MATCH LOADER
+  *******************/
+  chassis.setPose(0, 0, chassis.getPose().theta);
+  matcher.set_value(1);
+  chassis.moveToPose(-50, -3, 270, 2000, {.maxSpeed = 70, .minSpeed = 50});
+  hoodlum.move(0);
+  pros::delay(2500);
 
-   /******************
-   SCORE LONG GOAL
-   *******************/
-   chassis.moveToPoint(chassis.getPose().x + 30, chassis.getPose().y + 2, 2500,
-                       {.forwards = false, .maxSpeed = 70, .minSpeed = 50});
-   pros::delay(1000);
-   hoodlum.move(127);
-   pros::delay(1500);
+  /******************
+  SCORE LONG GOAL
+  *******************/
+  chassis.moveToPoint(chassis.getPose().x + 30, chassis.getPose().y + 2, 2500,
+                      {.forwards = false, .maxSpeed = 70, .minSpeed = 50});
+  pros::delay(1000);
+  hoodlum.move(127);
+  pros::delay(1500);
 
-   /******************
-   MOVE TO OTHER END OF LONG GOAL
-   *******************/
-   chassis.setPose(0, 0, chassis.getPose().theta);
-   pros::delay(100);
-   chassis.moveToPoint(-20, -3, 500, {.maxSpeed = 70, .minSpeed = 50});
-   chassis.moveToPoint(-15, 0, 1000, {.forwards = false, .minSpeed = 70});
-   chassis.moveToPose(0, -10, 90, 1000, {.minSpeed = 70, .earlyExitRange = 2});
-   chassis.moveToPoint(chassis.getPose().x + 76, chassis.getPose().y - 20, 3000,
-                       {.minSpeed = 80, .earlyExitRange = 18});
-   chassis.moveToPose(86, 7, 90, 2000);
+  /******************
+  MOVE TO OTHER END OF LONG GOAL
+  *******************/
+  chassis.setPose(0, 0, chassis.getPose().theta);
+  pros::delay(100);
+  chassis.moveToPoint(-20, -3, 500, {.maxSpeed = 70, .minSpeed = 50});
+  chassis.moveToPoint(-15, 0, 1000, {.forwards = false, .minSpeed = 70});
+  chassis.moveToPose(0, -10, 90, 1000, {.minSpeed = 70, .earlyExitRange = 2});
+  chassis.moveToPoint(chassis.getPose().x + 76, chassis.getPose().y - 20, 3000,
+                      {.minSpeed = 80, .earlyExitRange = 18});
+  chassis.moveToPose(86, 7, 90, 2000);
 
-   /******************
-   CLEAR BLUE LEFT MATCHLOADER
-   *******************/
-   hoodlum.move(0);
-   chassis.turnToPoint(100, 7, 1000);
-   chassis.moveToPoint(50, chassis.getPose().y - 4, 1000,
-                       {.forwards = false, .maxSpeed = 70, .minSpeed = 50});
-   chassis.moveToPose(100, chassis.getPose().y - 3, 90, 2000,
-                      {.maxSpeed = 70, .minSpeed = 50});
-   pros::delay(2500);
-   /******************
-   SCORE LONG GOAL
-   *******************/
-   chassis.moveToPoint(50, chassis.getPose().y, 1000, // y: 3 >> 5 >> 0
-                       {.forwards = false, .maxSpeed = 70, .minSpeed = 50});
-   pros::delay(1000);
-   hoodlum.move(127);
-   pros::delay(1500);
+  /******************
+  CLEAR BLUE LEFT MATCHLOADER
+  *******************/
+  hoodlum.move(0);
+  chassis.turnToPoint(100, 7, 1000);
+  chassis.moveToPoint(50, chassis.getPose().y, 1000,
+                      {.forwards = false, .maxSpeed = 70, .minSpeed = 50});
+  chassis.moveToPose(100, chassis.getPose().y, 90, 2000,
+                     {.maxSpeed = 70, .minSpeed = 50});
+  pros::delay(2500);
+  /******************
+  SCORE LONG GOAL
+  *******************/
+  chassis.moveToPoint(50, chassis.getPose().y, 1000,
+                      {.forwards = false, .maxSpeed = 70, .minSpeed = 50});
+  pros::delay(1000);
+  hoodlum.move(127);
+  pros::delay(500);
+  hoodlum.move(0);
 
   /******************
   TRACK DISTANCE FROM WALL
@@ -100,18 +101,20 @@ void run_auto_skills() {
   pros::delay(100);
   chassis.moveToPoint(10, 0, 1000);
   chassis.turnToHeading(0, 1000);
-  chassis.moveToPose(10, -60, 0, 1500, {.forwards = false, .minSpeed = 100, .earlyExitRange=1},false);
+  chassis.moveToPose(10, -60, 0, 1500,
+                     {.forwards = false, .minSpeed = 100, .earlyExitRange = 1},
+                     false);
   while (true) {
     int dist = lookout.get_distance();
-    if((dist == PROS_ERR)|| dist>750){
-    chassis.moveToPoint(chassis.getPose().x, chassis.getPose().y - 2, 1000,
-                        {.forwards = false,
-                         .maxSpeed = 40,
-                         .minSpeed = 50,
-                         .earlyExitRange = 0.5},
-                        false);
-    }else {
-      chassis.turnToHeading(90, 1000,{},false);
+    if ((dist == PROS_ERR) || dist > 750) {
+      chassis.moveToPoint(chassis.getPose().x, chassis.getPose().y - 2, 1000,
+                          {.forwards = false,
+                           .maxSpeed = 40,
+                           .minSpeed = 50,
+                           .earlyExitRange = 0.5},
+                          false);
+    } else {
+      chassis.turnToHeading(90, 1000, {}, false);
       break;
     }
   }
@@ -119,11 +122,36 @@ void run_auto_skills() {
   /******************
   SCORE LONG GOAL
   *******************/
-  // chassis.moveToPoint(35, chassis.getPose().y + 8, 2500,
-  //                     {.forwards = false, .maxSpeed = 70, .minSpeed = 50});
-  // pros::delay(1000);
-  // hoodlum.move(127);
-  // pros::delay(1000);
+  chassis.moveToPoint(-35, chassis.getPose().y, 1000,
+                      {.forwards = false, .maxSpeed = 70, .minSpeed = 50});
+  pros::delay(1000);
+  hoodlum.move(127);
+  pros::delay(1000);
+
+  /******************
+  CLEAR BLUE RIGHT MATCHLOADER
+  *******************/
+  chassis.setPose(0, 0, chassis.getPose().theta);
+  pros::delay(100);
+  hoodlum.move(0);
+  chassis.moveToPoint(35, 2, 1000);
+  pros::delay(2500);
+  chassis.moveToPoint(-35, 0, 1000,
+                      {.forwards = false, .maxSpeed = 70, .minSpeed = 50});
+  pros::delay(1000);
+
+  /******************
+  MOVE TO OTHER END OF LONG GOAL
+  *******************/
+  chassis.setPose(0, 0, chassis.getPose().theta);
+  pros::delay(100);
+  chassis.moveToPoint(20, -3, 500, {.maxSpeed = 70, .minSpeed = 50});
+  chassis.moveToPoint(15, 0, 1000, {.forwards = false, .minSpeed = 70});
+  chassis.moveToPose(0, -10, 270, 1000, {.minSpeed = 70, .earlyExitRange = 2});
+  chassis.moveToPoint(chassis.getPose().x - 76, chassis.getPose().y - 20, 3000,
+                      {.minSpeed = 80, .earlyExitRange = 18});
+  chassis.moveToPose(-86, -7, 90, 2000);
+  
 }
 //
 //
